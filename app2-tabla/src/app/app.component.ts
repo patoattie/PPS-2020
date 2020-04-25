ae6e941b21298bc9ff530cbe1af6f5516e93e586
patoattie <patoattie@gmail.com>
2020-04-25 11:06:25 -0300

    tabla didactica

--- app2-tabla/src/app/app.component.ts
+++ app2-tabla/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { LoginService } from './servicios/login.service';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private login: LoginService
   ) {
     this.initializeApp();
   }
@@ -32,6 +34,10 @@ export class AppComponent {
         }, 6750);
       }
     });
+  }
+
+  public estaLogueado(): boolean {
+    return this.login.getLogin();
   }
 
   /*hideSplashScreen() {
