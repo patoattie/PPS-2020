a7ce78af5ad3647ec5e536d6eb26a9d2635ceda5
patoattie <patoattie@gmail.com>
2020-05-09 12:39:44 -0300

    alarma de robo

--- app3-alarma/src/app/app.module.ts
+++ app3-alarma/src/app/app.module.ts
@@ -21,7 +21,7 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { MenuComponent } from './componentes/menu/menu.component';
-import { SelectorPrincipalComponent } from './componentes/selector-principal/selector-principal.component';
+// import { SelectorPrincipalComponent } from './componentes/selector-principal/selector-principal.component';
 import { HomeComponent } from './componentes/home/home.component';
 import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
 import { LogoComponent } from './componentes/logo/logo.component';
@@ -34,7 +34,7 @@ import { SelectorUsuarioComponent } from './componentes/selector-usuario/selecto
     AppComponent,
     PrincipalComponent,
     MenuComponent,
-    SelectorPrincipalComponent,
+    // SelectorPrincipalComponent,
     HomeComponent,
     ToolbarComponent,
     LogoComponent,
